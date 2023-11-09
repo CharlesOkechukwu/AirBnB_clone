4f8bb7a7986d3646abe3841b92eca1ff75669b30
Abdullahi Husain <abdulmohusain@gmail.com>
2023-11-09 11:31:42 +0100

    Alx

--- models/base_model.py
+++ models/base_model.py
@@ -4,16 +4,17 @@ This module contains BaseModel that defines all common
 attributes/methods for other classes.
 """
 import re
-from datetime import datetime, date, time
+from datetime import datetime
+import models
 from uuid import uuid4
 
-class BaseModel:
+
+class BaseModel():
     """
     defines all common attributes/methods for other classes.
     """
     def __init__(self, *args, **kwargs):
         """ Instance method"""
-        
         if kwargs or len(kwargs) != 0:
             for key, value in kwargs.items():
                 if key == "__class__":
@@ -35,17 +36,26 @@ class BaseModel:
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
 
+        for key, value in models.storage.all().items():
+            if self.id == value.id:
+                return
+        models.storage.new(self)
+
     def __str__(self):
         """ A nicely printable string representation of the
             object BaseModel
         """
-        return "[BaseModel] ({:s}) {}".format(self.id, self.__dict__)
+        dict_display = self.__dict__.copy()
+        if "__class__" in dict_display.keys():
+            dict_display.pop("__class__")
+        return "[BaseModel] ({:s}) {}".format(self.id, dict_display)
 
     def save(self):
         """updates the public instance attribute updated_at with
             the current datetime
         """
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """ returns a dictionary containing all keys/values of
@@ -56,28 +66,3 @@ class BaseModel:
         dict_copy['created_at'] = dict_copy['created_at'].isoformat()
         dict_copy['updated_at'] = dict_copy['updated_at'].isoformat()
         return dict_copy
-
-
-if __name__ == "__main__":
-
-    my_model = BaseModel()
-    my_model.name = "My_First_Model"
-    my_model.my_number = 89
-    print(my_model.id)
-    print(my_model)
-    print(type(my_model.created_at))
-    print("--")
-    my_model_json = my_model.to_dict()
-    print(my_model_json)
-    print("JSON of my_model:")
-    for key in my_model_json.keys():
-        print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
-
-    print("--")
-    my_new_model = BaseModel(**my_model_json)
-    print(my_new_model.id)
-    print(my_new_model)
-    print(type(my_new_model.created_at))
-
-    print("--")
-    print(my_model is my_new_model)
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,27 +1,41 @@
 #!/usr/bin/python3
 """ This module contains storage """
 import json
+from models.base_model import BaseModel
+
 
 class FileStorage:
     """This defines the class FileStorage"""
     __file_path = "file.json"
     __objects = {}
 
-    def __init__(self):
-        """Instance method"""
-
-
     def all(self):
         """ Returns the dictionary __objects """
         return self.__objects
-    
+
     def new(self, obj):
         """sets in __objects the obj with key <obj class name>.id"""
-        self.__objects[obj.__class__ + obj.id] = obj.to_dict()
-    
+        key = str(obj).split()[0].strip("[]") + '.' + obj.id
+        self.__objects[key] = obj
+
     def save(self):
         """serializes __objects to the JSON file (path: __file_path)"""
-        return
+        temp = {}
+        for key, value in self.__objects.items():
+            temp[key] = value.to_dict()
+        json_string = json.dumps(temp)
+        # validate length
+
+        with open(self.__file_path, "w") as file:
+            file.write(json_string)
+
     def reload(self):
-        """"""
-        return
+        """ deserializes the JSON file to __objects (only if the JSON file
+        (__file_path) exists otherwise it does nothing."""
+        try:
+            with open(self.__file_path, "r") as file:
+                json_dict = json.load(file)
+            for key, value in json_dict.items():
+                self.__objects[key] = (BaseModel(**value))
+        except FileNotFoundError:
+            return
