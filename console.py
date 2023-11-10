d8fad7d9ead8fe62b752be189d0c80dab0e70a9e
CharlesOkechukwu <realcharlieok@gmail.com>
2023-11-10 04:42:36 -0800

    create new instance and save

--- console.py
+++ console.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Create Custom Console for HBNB project"""
 import cmd
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
@@ -9,6 +10,7 @@ class HBNBCommand(cmd.Cmd):
     and delete
     """
     prompt = '(hbnb) '
+    __class = ['BaseModel']
 
     def do_EOF(self, line):
         """Handle end of file input"""
@@ -22,6 +24,18 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """handle when no input + ENTER is hit"""
         pass
+
+    def do_create(self, line):
+        """create instance of a class and saves it as json"""
+        arg = self.parseline(line)[0]
+        if arg is None:
+            print("** class name missing **")
+        elif arg and arg not in self.__class:
+            print("** class doesn't exist **")
+        else:
+            new_inst = eval(arg)()
+            new_inst.save()
+            print(new_inst.id)
 
 
 if __name__ == '__main__':
