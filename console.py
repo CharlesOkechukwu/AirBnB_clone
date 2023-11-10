827b4e0fa989f0a8cff2e4774080ef15e7d324f7
CharlesOkechukwu <realcharlieok@gmail.com>
2023-11-10 11:46:30 -0800

    destroy an instance

--- console.py
+++ console.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """Create Custom Console for HBNB project"""
 import cmd
-import models
 from models.base_model import BaseModel
+from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
@@ -50,11 +50,30 @@ class HBNBCommand(cmd.Cmd):
             print("** instance id missing **")
         else:
             key = arg1 + '.' + arg2
-            instance = models.storage.all().get(key)
+            instance = storage.all().get(key)
             if instance is None:
                 print("** no instance found **")
             else:
                 print(instance)
+
+    def do_destroy(self, line):
+        """delete an instance of a class"""
+        arg1 = self.parseline(line)[0]
+        arg2 = self.parseline(line)[1]
+        if arg1 is None:
+            print("** class name missing **")
+        elif arg1 not in self.__class:
+            print("** class doesn't exist **")
+        elif arg2 == "":
+            print("** instance id is missing **")
+        else:
+            key = arg1 + '.' + arg2
+            instance = storage.all().get(key)
+            if instance is None:
+                print("** no instance found **")
+            else:
+                del storage.all()[key]
+                storage.save()
 
 
 if __name__ == '__main__':
