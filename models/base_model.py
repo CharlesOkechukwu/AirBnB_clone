eb231372114f458db27f2a43d28b4d8bfb6e0fc2
Abdullahi Husain <abdulmohusain@gmail.com>
2023-11-13 16:01:21 +0100

    Alx

--- models/base_model.py
+++ models/base_model.py
@@ -41,6 +41,7 @@ class BaseModel():
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             models.storage.new(self)
+            models.storage.save()
 
     def __str__(self):
         """ A nicely printable string representation of the
